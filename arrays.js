248dc2ddf65217941b7a82d8dde2a4da1a5a7122
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-01-17 02:20:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -15,3 +15,12 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index]
 }
+
+function destructivelyRemoveElementFrom BeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
