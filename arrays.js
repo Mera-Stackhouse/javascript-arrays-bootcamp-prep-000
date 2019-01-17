e185eea4f37ea50fefac3bdcb1621d6de1fb5937
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-01-17 02:12:05 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,17 @@
 //var chocolateBars = new Array();
 
-var chocolateBars = [ 'snickers', 'hundred grand', 'kitkat', 'skittles'
+var chocolateBars = [ 'snickers', 'hundred grand', 'kitkat', 'skittles']
 
-]
+function addElementToEndOfArray(array, element) {
+  var newArray = [...array, element]
+  return newArray
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
